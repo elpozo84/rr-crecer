4aa39454765bd241aa2e6f9e1e40662c1196299d
elpozo84 <99618262+elpozo84@users.noreply.github.com>
2026-09-22 13:53:01 +0200

    Create PuenteRR.js

---
+++ app/components/PuenteRR.js
@@ -0,0 +1,8 @@
+<PuenteRR
+  tipo="conversacion"
+  desde="PESA 09"
+  hacia="PESA 01"
+  titulo="¿Qué significa «follar»?"
+  texto="Si esta es ahora la pregunta de tu hijo, puedes entrenarla específicamente."
+  href="/prepararme/7-9/pesa-01"
+/>
